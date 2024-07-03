e162d6bcbaaf18958e48832b6b33ef9f5e3f2eb3
tastycarbonara <kevinaurelian1@gmail.com>
2024-07-03 20:55:07 +0700

    user table migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Model
 {
     use HasFactory, Notifiable;
 
@@ -18,7 +18,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'role',
+        'avatar',
         'password',
     ];
 
@@ -42,6 +43,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-        ];
+        ];
     }
 }
+
