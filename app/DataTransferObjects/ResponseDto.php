aa2e25b49a773ed989ac721ba69634dabff1474b
Syauqi <syauqifrizman@gmail.com>
2024-10-12 09:47:29 +0700

    feat: add response dto to make it easier return from a method

---
+++ app/DataTransferObjects/ResponseDto.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataTransferObjects;
+
+class ResponseDto
+{
+    public readonly bool $isSuccess;
+    public readonly string $message;
+    public readonly mixed $errors;
+    public readonly mixed $data;
+    public readonly int $codeResponse;
+
+    public function __construct(bool $isSuccess, string $message, $errors, $data, $codeResponse)
+    {
+        $this->isSuccess = $isSuccess;
+        $this->message = $message;
+        $this->errors = $errors;
+        $this->data = $data;
+        $this->codeResponse = $codeResponse;
+    }
+}
