050dbbb3a552418bd10b55e96e847ddc4c7bd7eb
Syauqi <syauqifrizman@gmail.com>
2024-11-30 13:05:40 +0700

    feat: add DatabaseSeeder and UserSeeder for initial database population

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,34 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin SMAGA',
+                'username' => 'admin',
+                'password' => Hash::make('admin'),
+                'role' => 'ADMIN',
+                'avatar' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Teacher SMAGA',
+                'username' => 'teacher',
+                'password' => Hash::make('teacher'),
+                'role' => 'TEACHER',
+                'avatar' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Student SMAGA',
+                'username' => 'student',
+                'password' => Hash::make('student'),
+                'role' => 'STUDENT',
+                'avatar' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
